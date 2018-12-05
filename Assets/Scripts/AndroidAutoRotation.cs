555ba6a643b74de396aef86c9b2e10c0e6b7b11e
Foysal Ahamed <thisisfoysal@gmail.com>
2018-12-05 16:20:14 +0600

    Added Android auto rotation bug fix

---
+++ Assets/Scripts/AndroidAutoRotation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class AndroidAutoRotation : MonoBehaviour
+{
+    void OnApplicationFocus(bool haveFocus)
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (haveFocus) ToggleAutoRotation();
+#endif
+    }
+
+    private static void ToggleAutoRotation()
+    {
+        var autoRotationOn = DeviceAutoRotationIsOn();
+        Screen.autorotateToPortrait = autoRotationOn;
+        Screen.autorotateToLandscapeLeft = autoRotationOn;
+        Screen.autorotateToLandscapeRight = autoRotationOn;
+
+        Screen.orientation = autoRotationOn
+            ? ScreenOrientation.AutoRotation
+            : ScreenOrientation.Portrait;
+    }
+
+    private static bool DeviceAutoRotationIsOn()
+    {
+        using (var actClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            var context = actClass.GetStatic<AndroidJavaObject>("currentActivity");
+            var systemGlobal = new AndroidJavaClass("android.provider.Settings$System");
+            var rotationOn = systemGlobal.CallStatic<int>("getInt",
+                context.Call<AndroidJavaObject>("getContentResolver"), "accelerometer_rotation");
+
+            return rotationOn == 1;
+        }
+    }
+}
